e20f8526929a818d5306a91c2e6f00a510b5004a
Conan Kunhwan Ahn <kunhwon@gmail.com>
2020-03-06 15:26:34 +0900

    [style, webpack] Style 관련 loader 추가 및 miniizer 추가 (#4)
    * [style] sass-loader, css-loader, style-loader 추가
    
    * [eslint] ESLint 설정 수정
    
    * [webpack] devtool: source-map 적용
    
    * [style] css extract plugin 추가
    
    * [style] postcss-loader 추가 및 autoprefixer 추가
    
    * [eslint] ESLint 설정 수정

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { resolve } = require('path');
 
-module.exports = {
+const webpackConfig = {
   mode: 'development',
+  devtool: 'source-map',
   entry: './src/main.js',
   output: {
     path: resolve(__dirname, 'dist'),
@@ -14,6 +18,54 @@ module.exports = {
       '@': resolve(__dirname, 'src'),
     },
   },
+  optimization: {
+    minimizer: [
+      new TerserJSPlugin({
+        terserOptions: {
+          compress: {
+            arrows: false,
+            collapse_vars: false,
+            comparisons: false,
+            computed_props: false,
+            hoist_props: false,
+            inline: false,
+            loops: false,
+            negate_iife: false,
+            properties: false,
+            reduce_funcs: false,
+            reduce_vars: false,
+            switches: false,
+            typeofs: false,
+          },
+          mangle: {
+            safari10: true,
+          },
+        },
+        sourceMap: true,
+        extractComments: false,
+      }),
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new OptimizeCSSAssetsPlugin({
+      cssProcessorPluginOptions: {
+        preset: [
+          'default',
+          {
+            mergeLonghand: false,
+            cssDeclarationSorter: false,
+          },
+        ],
+      },
+    }),
+    new MiniCssExtractPlugin({
+      ignoreOrder: true,
+    }),
+  ],
   module: {
     rules: [
       {
@@ -26,12 +78,21 @@ module.exports = {
           loader: 'babel-loader',
         },
       },
+      {
+        test: /\.scss$/,
+        exclude: /node_moduels/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+          'sass-loader',
+          'postcss-loader',
+        ],
+      },
     ],
   },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-  ],
 };
+
+
+module.exports = webpackConfig;

d81591b1d8a897e5f5610f0a7dfc2ab008da1781
Conan Kunhwan Ahn <kunhwon@gmail.com>
2020-03-07 00:56:45 +0900

    [vue] Add Vuex

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
 import router from './routes';
+import store from './store';
 import '@/styles/index.scss';
 
 import App from './App.vue';
 
 new Vue({
+  store,
   router,
   render: (h) => h(App),
 }).$mount('#app');

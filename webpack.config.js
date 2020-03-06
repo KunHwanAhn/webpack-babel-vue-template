4ca48a66074ac05108d510db6ec3b24fe9da7ee1
Conan Kunhwan Ahn <kunhwon@gmail.com>
2020-03-06 14:55:51 +0900

    [style] css extract plugin 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { resolve } = require('path');
 
 const webpackConfig = {
@@ -15,6 +18,54 @@ const webpackConfig = {
       '@': resolve(__dirname, 'src'),
     },
   },
+  optimization: {
+    minimizer: [
+      new TerserJSPlugin({
+        terserOptions: {
+          compress: {
+            arrows: false,
+            collapse_vars: false,
+            comparisons: false,
+            computed_props: false,
+            hoist_props: false,
+            inline: false,
+            loops: false,
+            negate_iife: false,
+            properties: false,
+            reduce_funcs: false,
+            reduce_vars: false,
+            switches: false,
+            typeofs: false,
+          },
+          mangle: {
+            safari10: true,
+          },
+        },
+        sourceMap: true,
+        extractComments: false,
+      }),
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new OptimizeCSSAssetsPlugin({
+      cssProcessorPluginOptions: {
+        preset: [
+          'default',
+          {
+            mergeLonghand: false,
+            cssDeclarationSorter: false,
+          },
+        ],
+      },
+    }),
+    new MiniCssExtractPlugin({
+      ignoreOrder: true,
+    }),
+  ],
   module: {
     rules: [
       {
@@ -31,19 +82,15 @@ const webpackConfig = {
         test: /\.scss$/,
         exclude: /node_moduels/,
         use: [
-          'style-loader',
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
           'css-loader',
           'sass-loader',
         ],
       },
     ],
   },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-  ],
 };
 
 
